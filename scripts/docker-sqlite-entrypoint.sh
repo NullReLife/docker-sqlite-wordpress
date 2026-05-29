71b1d0a5ecc271636d6d9c6c4fcb3b2b0e2468d8
NullReLife <3126385134@qq.com>
2026-05-29 16:00:59 +0800

    Raise WordPress plugin upload limits

--- scripts/docker-sqlite-entrypoint.sh
+++ scripts/docker-sqlite-entrypoint.sh
@@ -6,6 +6,26 @@ WP_SOURCE_DIR="${WP_PREPARE_DIR:-/usr/src/wordpress}"
 WP_OWNER="${WORDPRESS_FILE_OWNER:-www-data:www-data}"
 WP_DIR_MODE="${WORDPRESS_DIR_MODE:-755}"
 WP_FILE_MODE="${WORDPRESS_FILE_MODE:-644}"
+WORDPRESS_UPLOAD_MAX_FILESIZE="${WORDPRESS_UPLOAD_MAX_FILESIZE:-256M}"
+WORDPRESS_POST_MAX_SIZE="${WORDPRESS_POST_MAX_SIZE:-256M}"
+WORDPRESS_PHP_MEMORY_LIMIT="${WORDPRESS_PHP_MEMORY_LIMIT:-512M}"
+WORDPRESS_MEMORY_LIMIT="${WORDPRESS_MEMORY_LIMIT:-256M}"
+WORDPRESS_MAX_MEMORY_LIMIT="${WORDPRESS_MAX_MEMORY_LIMIT:-512M}"
+WORDPRESS_MAX_EXECUTION_TIME="${WORDPRESS_MAX_EXECUTION_TIME:-300}"
+WORDPRESS_MAX_INPUT_TIME="${WORDPRESS_MAX_INPUT_TIME:-300}"
+
+configure_php_upload_limits() {
+  local ini_dir="${PHP_INI_DIR:-/usr/local/etc/php}/conf.d"
+  mkdir -p "${ini_dir}"
+
+  cat > "${ini_dir}/docker-wordpress-upload-limits.ini" <<EOF
+upload_max_filesize = ${WORDPRESS_UPLOAD_MAX_FILESIZE}
+post_max_size = ${WORDPRESS_POST_MAX_SIZE}
+memory_limit = ${WORDPRESS_PHP_MEMORY_LIMIT}
+max_execution_time = ${WORDPRESS_MAX_EXECUTION_TIME}
+max_input_time = ${WORDPRESS_MAX_INPUT_TIME}
+EOF
+}
 
 insert_filesystem_constants() {
   local config_file="$1"
@@ -17,7 +37,7 @@ insert_filesystem_constants() {
   block_file="$(mktemp)"
   tmp_file="$(mktemp)"
 
-  cat > "${block_file}" <<'EOF'
+  cat > "${block_file}" <<EOF
 /* Docker SQLite WordPress filesystem settings. */
 if ( ! defined( 'FS_METHOD' ) ) {
 	define( 'FS_METHOD', 'direct' );
@@ -27,6 +47,12 @@ if ( ! defined( 'FS_CHMOD_DIR' ) ) {
 }
 if ( ! defined( 'FS_CHMOD_FILE' ) ) {
 	define( 'FS_CHMOD_FILE', ( 0644 & ~ umask() ) );
+}
+if ( ! defined( 'WP_MEMORY_LIMIT' ) ) {
+	define( 'WP_MEMORY_LIMIT', '${WORDPRESS_MEMORY_LIMIT}' );
+}
+if ( ! defined( 'WP_MAX_MEMORY_LIMIT' ) ) {
+	define( 'WP_MAX_MEMORY_LIMIT', '${WORDPRESS_MAX_MEMORY_LIMIT}' );
 }
 EOF
 
@@ -72,6 +98,7 @@ prepare_writable_wp_content() {
   done
 }
 
+configure_php_upload_limits
 insert_filesystem_constants "${WP_SOURCE_DIR}/wp-config.php"
 insert_filesystem_constants "${WP_RUNTIME_DIR}/wp-config.php"
 prepare_writable_wp_content "${WP_SOURCE_DIR}"
